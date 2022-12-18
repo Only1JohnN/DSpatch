720965acea822c0efec4e1fab80c4b6f84b215a0
Zeerex23 <tobilobadiekola@gmail.com>
2022-12-18 01:07:15 +0100

    feat: added Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Navbar } from "./components";
+import { Navbar, Footer } from "./components";
 import { LandingPage } from "./pages";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
       <LandingPage />
+      <Footer />
     </div>
   );
 }
