86abe5e7fe33a5ef39d4fc83e12c97d41eb8a0f7
Zeerex23 <tobilobadiekola@gmail.com>
2023-01-21 06:56:55 +0100

    refactor: added About page to Routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes as DOMRoutes, Route } from "react-router-dom";
 
-import { LandingPage, Terms, Tracking } from "./pages";
+import { LandingPage, Terms, Tracking, About } from "./pages";
 
 const Routes = ({ location }) => {
   return (
     <DOMRoutes location={location}>
       <Route exact path='/' element={<LandingPage />} />
-
       <Route exact path='/terms' element={<Terms />} />
       <Route exact path='/tracking' element={<Tracking />} />
+      <Route exact path='/about' element={<About />} />
     </DOMRoutes>
   );
 };
