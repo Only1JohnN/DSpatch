e2e3c697e9a7374359126b5eed0904b7b5dd5a46
Elijah Ndibe <elijahndibe.com>
2023-02-20 19:59:57 +0100

    modified: refactored help icons

--- src/pages/Help/styles.js
+++ src/pages/Help/styles.js
@@ -1,18 +1,15 @@
-
-
 import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme) => ({
-    alignment: {
-        width: "55px",
-        margin: "auto",
-        
-
-    },
-
-    imageMockup: {
-        width: "46px",
-        height: "56px",
-        marginTop: "30px"
-    }
+  alignment: {
+    marginTop: "50px",
+    paddingTop: "30px",
+  },
+  imageMockup: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50px",
+    height: "56px",
+  },
 }));
